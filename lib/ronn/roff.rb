74a1321f619bb7962c3e36c2071406fe85625dbc
Andrew Janke <floss@apjanke.net>
2019-01-07 22:30:48 -0500

    Don't allow URLs to be hyphenated

--- lib/ronn/roff.rb
+++ lib/ronn/roff.rb
@@ -277,13 +277,13 @@ module Ronn
             inline_filter(node.children)
           elsif node.has_attribute?('data-bare-link')
             write '\fI'
-            inline_filter(node.children)
+            write '\%' + escape(node.attributes['href'].content)
             write '\fR'
           else
             inline_filter(node.children)
             write ' '
             write '\fI'
-            write escape(node.attributes['href'].content)
+            write '\%' + escape(node.attributes['href'].content)
             write '\fR'
           end
 
