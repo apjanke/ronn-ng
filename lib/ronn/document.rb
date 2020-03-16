apjanke/ronn-ng

commit 759cc186fdb45a509e8d9eb4421a81122b9d96d8

Encoding: use UTF-8 by default, add a -E option to specify it

lib/ronn/document.rb
@@ -23,6 +23,9 @@ module Ronn
     # object is created with a stream, in which case stdin will be read.
     attr_reader :path
 
+    # Encoding that the Ronn document is in
+    attr_accessor :encoding
+
     # The raw input data, read from path or stream and unmodified.
     attr_reader :data
 
@@ -67,13 +70,14 @@ module Ronn
     # for any writeable attributes defined on this class.
     def initialize(path = nil, attributes = {}, &block)
       @path = path
+      attributes.each { |attr_name, value| send("#{attr_name}=", value) }
       @basename = path.to_s =~ /^-?$/ ? nil : File.basename(path)
       @reader = block ||
                 lambda do |f|
                   if ['-', nil].include?(f)
-                    STDIN.read
+                    STDIN.read()
                   else
-                    File.read(f)
+                    File.read(f, :encoding => @encoding)
                   end
                 end
       @data = @reader.call(path)
@@ -84,8 +88,6 @@ module Ronn
       @markdown, @input_html, @html = nil
       @index = Ronn::Index[path || '.']
       @index.add_manual(self) if path && name
-
-      attributes.each { |attr_name, value| send("#{attr_name}=", value) }
     end
 
     # Generate a file basename of the form "<name>.<section>.<type>"

bin/ronn
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -E UTF-8
 # / Usage: ronn <options> <file>...
 # /        ronn -m|--man <file>
 # /        ronn -S|--server <file> ...
@@ -31,6 +31,9 @@
 # /   -W                        disable previously enabled troff warnings
 # /       --version             show ronn version and exit
 # /       --help                show this help message
+# /   -E <encoding>             specify the encoding files are in (default is UTF-8)
+# /                             Note: this only affects files. Data passed in to ronn's STDIN
+# /                             must be in UTF-8 regardless of the -E setting.
 # /
 # / A <file> named example.1.ronn generates example.1.html (HTML manpage)
 # / and example.1 (roff manpage) by default.
@@ -75,6 +78,7 @@ view    = false
 server  = false
 port_arg = nil
 formats = nil
+encoding = "UTF-8"
 options = {}
 write_index = false
 styles = %w[man]
@@ -111,6 +115,7 @@ ARGV.options do |argv|
   argv.on('-f', '--fragment') { (formats ||= []) << 'html_fragment' }
   argv.on('--markdown')       { (formats ||= []) << 'markdown' }
   argv.on('--yaml')           { (formats ||= []) << 'yaml' }
+  argv.on('-E', '--encoding=V') { |val| encoding = val }
 
   # html output options
   argv.on('-s', '--style=V')  { |val| styles += val.split(/[, \n]+/) }
@@ -158,6 +163,7 @@ formats.delete('html') if formats.include?('html_fragment')
 options['date'] &&= Date.strptime(options['date'], '%Y-%m-%d')
 options['styles'] = styles
 options['outdir'] = output_dir
+options['encoding'] = encoding
 
 unless port_arg.nil?
   begin

man/ronn.1.ronn
@@ -7,7 +7,8 @@ ronn(1) -- convert markdown files to manpages
 `ronn` `-m`|`--man` <file>...<br>
 `ronn` `-S`|`--server` <file>...<br>
 `ronn` `--pipe` <file><br>
-`ronn` &lt; <file>
+`ronn` &lt; <file><br>
+`ronn` `-E`|`--encoding` <encoding> ...
 
 ## DESCRIPTION
 
@@ -34,6 +35,9 @@ from standard input and roff output is written to standard output. Use the
 The `ronn` command expects input to be valid ronn-format(7) text.  Source files
 are typically named <name>.<section>.ronn (e.g., `example.1.ronn`).  The <name>
 and <section> should match the name and section defined in the <file>'s heading.
+
+Source files must be in UTF-8 encoding, or the encoding specified by the
+`-E`/`--encoding` option, regardless of the locale that `ronn` is running under.
 
 When building roff or HTML output files, destination filenames are determined by
 taking the basename of the input <file> and adding the appropriate file
@@ -78,6 +82,11 @@ directly to a man pager.
   * `-o`=<directory>, `--output-dir`=<directory>:
     Write generated files to the specified directory instead of the default
     location.
+  
+  * `-E`=<encoding>, `--encoding`=<encoding:
+    Specify the encoding that input files are in. Default is UTF-8, regardless
+    of user's locale settings. Input sent to STDIN is always treated as UTF-8,
+    regardless of whether `-E` is passed.
 
 Format options control the files `ronn` generates, or the output format when the
 `--pipe` argument is specified. When no format options are given, both `--roff`

ronn-ng.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name = 'ronn-ng'
   s.version = '0.9.1'
-  s.date = '2019-12-21'
+  s.date = '2020-01-29'
 
   s.summary     = 'Builds man pages from Markdown'
   s.description = 'Ronn-NG builds manuals in HTML and Unix man page format from Markdown.'

commit 2619c43f9564c6763149593c0a4d5c19a419fcc1

Fix tests
They were broken by 759cc186fdb45a509e8d9eb4421a81122b9d96d8.

This commit keeps the changes made there, only reverting the specific
bit that broke the test suite.

lib/ronn/document.rb
@@ -70,7 +70,6 @@ module Ronn
     # for any writeable attributes defined on this class.
     def initialize(path = nil, attributes = {}, &block)
       @path = path
-      attributes.each { |attr_name, value| send("#{attr_name}=", value) }
       @basename = path.to_s =~ /^-?$/ ? nil : File.basename(path)
       @reader = block ||
                 lambda do |f|
@@ -88,6 +87,8 @@ module Ronn
       @markdown, @input_html, @html = nil
       @index = Ronn::Index[path || '.']
       @index.add_manual(self) if path && name
+
+      attributes.each { |attr_name, value| send("#{attr_name}=", value) }
     end
 
     # Generate a file basename of the form "<name>.<section>.<type>"